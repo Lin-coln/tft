d19176af9aae60d4b7f6f0236ed15a63328b7878
lincoln <1043156036@qq.com>
2026-08-29 16:55:10 +0800

    chore: format

--- scripts/resolve_info.ts
+++ scripts/resolve_info.ts
@@ -1,14 +1,11 @@
-
-
-
 const images = [
-  './assets/images/img_1.png',
-  './assets/images/img_2.png',
-  './assets/images/img_3.png',
-  './assets/images/img_4.png',
-]
+  "./assets/images/img_1.png",
+  "./assets/images/img_2.png",
+  "./assets/images/img_3.png",
+  "./assets/images/img_4.png",
+];
 
 for (const img of images) {
   const text = await Bun.$`echo ${img} | tft resolve`.text();
-  console.log(JSON.stringify(JSON.parse(text), null , 2));
+  console.log(JSON.stringify(JSON.parse(text), null, 2));
 }
